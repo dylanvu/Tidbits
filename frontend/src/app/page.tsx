2a6dc5f0639809c37778224194a393320bfcc6ab
Dylan Vu <dylanvu9@gmail.com>
2024-03-30 12:57:42 -0400

    sass works for sure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import styles from "@/styles/Home.module.sass";
 
 export default function Home() {
   const router = useRouter();
   return (
     <main>
-      <div>
+      <div className={styles.test}>
         Here is where you would upload media to be turned into tidbits
       </div>
       <button type="button" onClick={() => router.push('/browse')}>
