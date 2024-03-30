6af092c197c7ffb8ee14c644100743287fc7385a
Dylan Vu <dylanvu9@gmail.com>
2024-03-30 17:42:27 -0400

    tag system clicking works

--- frontend/src/app/browse/course.tsx
+++ frontend/src/app/browse/course.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 // TODO: retrieve this from the backend
 let tidbitPreviews: { title: string; duration: number }[] = Array.from({
@@ -11,12 +14,45 @@ tidbitPreviews.fill({
     duration: 45,
 });
 
-// duplicate this
+// TODO: retrieve this from the backend
+const tags = ["algorithms", "data structures", "system design"];
 
 function CourseUI({ course }: { course: string }) {
+    const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set());
     return (
         <div>
-            <div>Reels for {course}</div>
+            <div>{course}</div>
+            <div>
+                {/* show all the tags associated with the course */}
+                {tags.map((tag) => (
+                    <button
+                        onClick={() => {
+                            if (selectedTags.has(tag)) {
+                                // remove it
+                                const deepClonedSet: Set<string> = new Set(
+                                    JSON.parse(
+                                        JSON.stringify([...selectedTags]),
+                                    ),
+                                );
+                                deepClonedSet.delete(tag);
+                                setSelectedTags(deepClonedSet);
+                            } else {
+                                // add it
+                                // deep clonse
+                                const deepClonedSet: Set<string> = new Set(
+                                    JSON.parse(
+                                        JSON.stringify([...selectedTags, tag]),
+                                    ),
+                                );
+                                setSelectedTags(deepClonedSet);
+                            }
+                        }}
+                        className={selectedTags.has(tag) ? "selected" : ""}
+                    >
+                        {tag}
+                    </button>
+                ))}
+            </div>
             {tidbitPreviews.map((tidbit, index) => (
                 <Link href="/view" key={tidbit.title + `-${index}`}>
                     <img src="./placeholder.png" />
