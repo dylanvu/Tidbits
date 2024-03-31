ffb705cf308bd867012e4adeee13776e5ae84684
Jasmine <jasmineqw45@gmail.com>
2024-03-30 20:40:09 -0400

    basic set up for cards

--- frontend/src/components/DailyTidbit.tsx
+++ frontend/src/components/DailyTidbit.tsx
@@ -11,14 +11,24 @@ function DailyTidbit({ tidbit }: { tidbit: IDailyTidbit }) {
     const router = useRouter();
     return (
         <div className={`${globalStyles.test} ${globalStyles.card}`}>
-            <div className={globalStyles.h1}>{tidbit.title}</div>
-            <div className={globalStyles.p}>{tidbit.course}</div>
-            <div className={globalStyles.iconTag}>
-                {" "}
-                <Clock color={globalStyles.$iconColor} size={48} />
-                <p>{tidbit.duration} seconds</p>
-            </div>
-            <img src="./placeholder.png"></img>
+           <div className={globalStyles.row}>
+                <div className={globalStyles.column}>
+                    <div className={globalStyles.h1}>{tidbit.title}</div>
+                    <div className={globalStyles.p}>{tidbit.course}</div>
+                   {/* icon */}
+                    <div className={globalStyles.iconTag}>
+                        {" "}
+                        <Clock color={globalStyles.$iconColor} size={22} />
+                        <p>{tidbit.duration} seconds</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="./bubblePlaceHolder.png"></img> 
+                </div>
+            </div> 
+            
+            
+           
             {/* TODO: link this to the real video */}
             <button
                 onClick={() => router.push("/view")}
